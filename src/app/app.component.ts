6a113b36eed5b6002b5fc72000c26f30a9dc2b5e
Sonia-Baibou <s.baibou@ohmycode.io>
2023-04-21 22:29:08 +0200

    boilerplate ionic angular standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
 })
 export class AppComponent {
   constructor() {}
