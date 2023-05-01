0a53234a42292216ec2999927c19cbbf5d25b934
Sonia-Baibou <s.baibou@ohmycode.io>
2023-05-01 23:06:17 +0200

    localStorage - partial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -22,24 +22,36 @@ import { FormComponent } from '../shared/components/form/form.component';
 })
 export class Tab1Page {
   public img!: any;
-  public explanation !: any;
+  public explanation!: any;
   public isRevealed = false;
+  public localStorageTab!: any[];
 
   constructor(
     private openAiService: OpenaiService,
     private metApiService: MetApiService
   ) {
-
- this.metApiService.refresh().subscribe((item) => {
-   console.log(item);
-   this.img = item;
- });
+    this.metApiService.refresh().subscribe((item) => {
+      console.log(item);
+      this.img = item;
+    });
     // subscribe to explanation$
-    this.metApiService.explanation$.subscribe(data=>{
-      this.explanation = data
-    })
+    this.metApiService.explanation$.subscribe((data) => {
+      this.explanation = data;
+    });
+
+    // check if localStorage exists
+    this.checkLocalStorageExists();
   }
 
+  public checkLocalStorageExists() {
+    if (localStorage.getItem('my-favorites-imgs')) {
+      this.localStorageTab = JSON.parse(
+        localStorage.getItem('my-favorites-imgs') ?? ''
+      );
+    } else {
+      this.localStorageTab = [];
+    }
+  }
 
   public messages = [
     {
@@ -50,18 +62,19 @@ export class Tab1Page {
   ];
 
   ionViewWillEnter() {
-    console.log('test depuis ionviewenter')
+    console.log('test depuis ionviewenter');
     this.img = '';
     this.explanation = '';
     this.isRevealed = false;
     console.log(this.img);
-this.metApiService.refresh().subscribe((item) => {
-  console.log(item);
-  this.img = item;
-});
+    this.metApiService.refresh().subscribe((item) => {
+      console.log(item);
+      this.img = item;
+      this.checkLocalStorageExists();
+    });
   }
 
-  public onNext(){
+  public onNext() {
     console.log('next');
     this.isRevealed = false;
     this.metApiService.refresh().subscribe((item) => {
@@ -69,7 +82,6 @@ this.metApiService.refresh().subscribe((item) => {
       this.img = item;
     });
   }
-
 
   // onExplain(title: string, painter: string, period: string) {
   //   // const newMessage = `Can you explain to a total beginner in painting the painting ${title} from ${painter}, painted in ${period}. The response should start directly with the answer without you telling me anything else.`;
@@ -82,8 +94,47 @@ this.metApiService.refresh().subscribe((item) => {
   //   // });
 
   // }
-  onExplain(){
+  onExplain() {
     // reveal
-    this.isRevealed = true
+    this.isRevealed = true;
+  }
+
+  onLike() {
+    console.log(this.img, 'thisimg');
+    //console.log(this.explanation, 'explanation');
+    const objSaved = {
+      objDetail: this.img,
+      explanation: this.explanation,
+    };
+    //console.log(objSaved);
+    this.checkObjAlreadySaved(objSaved);
+  }
+
+  public checkObjAlreadySaved(objSaved: any) {
+    console.log(this.localStorageTab);
+    if (this.localStorageTab.length > 0) {
+      const imgFound = this.localStorageTab.find(
+        (item) => item.objDetail.objectID === objSaved.objDetail.objectID
+      );
+      console.log(imgFound, 'imgFound');
+      if (imgFound) {
+        console.log(imgFound, 'already exists');
+        return;
+      } else {
+       console.log('new', objSaved.objDetail.objectID);
+       this.localStorageTab.push(objSaved);
+       localStorage.setItem(
+         'my-favorites-imgs',
+         JSON.stringify(this.localStorageTab)
+       );
+      }
+    } else if (this.localStorageTab.length === 0) {
+      console.log('new', objSaved.objDetail.objectID);
+      this.localStorageTab.push(objSaved);
+      localStorage.setItem(
+        'my-favorites-imgs',
+        JSON.stringify(this.localStorageTab)
+      );
+    }
   }
 }
